4a8e157f18062a8dd59c6ceb8c5e483c0250ead0
Ermal Dobratiqi <ermaldobratiqi@hotmail.com>
2023-04-06 04:53:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PersonalPortfolio-Ermal-Dobratiqi
+ Here is a personal portfolio of Ermal Dobratiqi that is done with HTML, CSS and JavaScript to describe the knowledge on these technologies.
---
+++ js/main.js
@@ -0,0 +1,73 @@
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+
+//Portfolio Section - Modal
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
+
+//Our Clients - Swiper
+
+
+//Website dark/light theme
+
+
+//Scroll to top button
+
+
+//Navigation menu items active on page scroll
+
+
+//Responsive navigation menu toggle
+
+
+//Scroll reveal animations
+
+
+//Common reveal options to create reveal animations
